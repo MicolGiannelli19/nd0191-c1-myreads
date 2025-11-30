e9800cbca04cb8ce6dc832eabd32e1a953a28e2b
MicolGiannelli19 <micol.giannelli@outlook.com>
2025-11-30 22:49:33 +0000

    included routing with react router dom

--- starter/src/App.js
+++ starter/src/App.js
@@ -2,16 +2,23 @@ import "./App.css";
 import { useState } from "react";
 import Book from "./components/Book";
 import SearchBooks from "./components/SearchBooks";
+import Home from "./components/Home";
+import {Route, Routes} from "react-router-dom"
 
 function App() {
   const [showSearchPage, setShowSearchpage] = useState(false);
 
   return (
-    <div className="app">
-      {showSearchPage ? (
-        <SearchBooks></SearchBooks>
-      ) : (
-  );
-}
+    <Routes>
+      <Route 
+      path = "/"
+      element = {<Home/>}
+      />
+      <Route
+      path="/search"
+      element = {<SearchBooks/>}
+      />
+    </Routes>
+  )
 
 export default App;
--- starter/src/components/SearchBooks.js
+++ starter/src/components/SearchBooks.js
@@ -1,17 +1,19 @@
 // This is my search books view
 import Book from "./Book";
+import { Link } from "react-router-dom";
 
 export default function SearchBooks() {
   return (
     <div className="search-books">
       <div className="search-books-bar">
-        <a
+        <Link to="/"> Back to home </Link>
+        {/* <a
           className="close-search"
           //   TODO change this to link functionlity
           onClick={() => setShowSearchpage(!showSearchPage)}
         >
           Close
-        </a>
+        </a> */}
         <div className="search-books-input-wrapper">
           <input type="text" placeholder="Search by title, author, or ISBN" />
         </div>
