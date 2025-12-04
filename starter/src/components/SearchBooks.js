0300d7188dade43aab6d3e6eaec670c4796e3282
MicolGiannelli19 <micol.giannelli@outlook.com>
2025-12-04 23:41:05 +0000

    Your message here

--- starter/src/components/SearchBooks.js
+++ starter/src/components/SearchBooks.js
@@ -2,8 +2,17 @@
 // import Book from "./Book";
 import { Link } from "react-router-dom";
 import TestApi from "./TestApi";
+import { useEffect, useState } from "react";
+import Book from "./Book";
+import { search } from "../BooksAPI";
 
 export default function SearchBooks() {
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    search().then((data) => console.log("books", data));
+  }, []);
+
   return (
     <div className="search-books">
       <Link className="close-search" to="/">
@@ -13,11 +22,17 @@ export default function SearchBooks() {
       <div className="search-books-input-wrapper">
         <input type="text" placeholder="Search by title, author, or ISBN" />
       </div>
-      <div className="search-books-bar">
-        <TestApi></TestApi>
-      </div>
+      <div className="search-books-bar"></div>
       <div className="search-books-results">
-        <ol className="books-grid"></ol>
+        <ol className="books-grid">
+          <li>
+            {books.map((book) => (
+              <li>
+                <Book key={books.id} book={book}></Book>
+              </li>
+            ))}
+          </li>
+        </ol>
       </div>
     </div>
   );
